0f9a6bfa5520f88772a0a4b0772091eaea691ebd
Samlxrd <samlord.dzn@gmail.com>
2024-04-07 21:31:37 -0300

    added database connection

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DBConnection } from './connection';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
+  DBConnection();
 }
 bootstrap();
