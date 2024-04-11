16e41432cc07f0f3758f64b38a061e9758245842
Samlxrd <samlord.dzn@gmail.com>
2024-04-10 23:36:34 -0300

    created database service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DbconnectionService } from './dbconnection/dbconnection.service';
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, DbconnectionService],
 })
 export class AppModule {}
