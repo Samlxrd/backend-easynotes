cd27ad51040907829e1159a51cb3b16ca6f3b089
Samlxrd <samlord.dzn@gmail.com>
2024-04-10 23:37:30 -0300

    removed provisory connection

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { DBConnection } from './connection';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
-  DBConnection();
 }
 bootstrap();
