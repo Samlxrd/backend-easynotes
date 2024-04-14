ca7a65c66deedd5afb0cd82fc148e7cefa7fb5cd
Samlxrd <samlord.dzn@gmail.com>
2024-04-13 21:39:28 -0300

    Feat: Created Note Module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DbconnectionService } from './dbconnection/dbconnection.service';
+import { NoteModule } from './note/note.module';
 
 @Module({
-  imports: [],
+  imports: [NoteModule],
   controllers: [AppController],
   providers: [AppService, DbconnectionService],
 })
